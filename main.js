790009d561662b7908d35d91d21274d49b8493be
RasyaN5 <roisya000@gmail.com>
2026-03-30 14:48:15 +0700

    cobaan

---
+++ main.js
@@ -0,0 +1,29 @@
+//studi kasuss lv1
+const prompt = require("prompt-sync")(); //PERM
+
+const USEERNAME = "admin" ;
+const PASSWORD = "12345";
+
+let username = prompt("Masukkan Username : ");
+let password = prompt("Masukkan Password : ");
+
+// if (nilai >= 75) {
+//     console.log("lulus");
+// } else if (nilai <= 75) {
+//     console.log("gagal");
+// }
+
+
+//studi kasus lv2
+// if (nilai >= 90) {
+//     console.log("A")
+// } else if (nilai >= 80 && nilai < 90) {
+//     console.log("B")
+// } else if (nilai >= 70 && nilai < 80) {
+//     console.log("C")
+// }else {
+//     console.log("D")
+// }
+
+//studi kasus lv3
+
